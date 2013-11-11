1b6c6ed4f25e3ec1fb09c50b0e621cfd0930db55
Gismar zum Felde <thegismar@gmail.com>
2013-11-11 16:55:30 -0300

    1st

---
+++ src/main/java/org/gismarzf/jevernote/LoginController.java
@@ -0,0 +1,15 @@
+package org.gismarzf.jevernote;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+
+public class LoginController {
+
+	@FXML
+	private Image jevernoteLogo;
+
+	public LoginController() {
+
+	}
+
+}
